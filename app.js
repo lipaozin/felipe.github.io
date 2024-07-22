e7b6fc4ccbadbac85aaa85c5260c324f06b02a57
Luis Felipe Cremonez <105985428+felipecremonez@users.noreply.github.com>
2024-07-22 12:13:38 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const menuToggle = document.querySelector('.menu-toggle');
-    const navList = document.querySelector('.nav-list');
+  const menuToggle = document.querySelector('.menu-toggle');
+  const navList = document.querySelector('.nav-list');
 
-    menuToggle.addEventListener('click', function() {
-        navList.classList.toggle('active');
-    });
+  menuToggle.addEventListener('click', function() {
+    navList.classList.toggle('active');
+  });
+
+  const navListItems = document.getElementsByClassName('nav-list-item');
+
+  for (const item of navListItems) {
+    item.addEventListener('click', () => {
+      navList.classList.toggle('active');
+    });    
+  }
 });
